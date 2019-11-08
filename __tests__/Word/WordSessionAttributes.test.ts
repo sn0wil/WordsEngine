5b90284719284665bd81c5d26cb61c1e5950e363
Alexander Denisevich <denisevich.aa@gmail.com>
2019-11-09 02:30:42 +0300

    feat(tests/Word): tests for Word class and children classes

---
+++ __tests__/Word/WordSessionAttributes.test.ts
@@ -0,0 +1,9 @@
+import { WordSessionAttributes } from '../../src/Word/WordSessionAttributes';
+
+test('clear should reset currectAnswers and incorrectAnswers values', () => {
+    const sessionAttributes = new WordSessionAttributes();
+    sessionAttributes.currectAnswers = 1;
+    sessionAttributes.incorrectAnswers = 1;
+    sessionAttributes.clear();
+    expect([0, 0]).toEqual([sessionAttributes.currectAnswers, sessionAttributes.incorrectAnswers]);
+});
