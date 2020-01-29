daba6aaace8b147d9134f7ff9e629f33a88721fd
Alexander Denisevich <denisevich.aa@gmail.com>
2020-01-29 11:09:46 +0300

    feat(word): new word class

--- src/Word/Word.ts
+++ src/Word/Word.ts
@@ -1,15 +1,57 @@
-import { WordGeneralAttributes } from './WordGeneralAttributes';
-import { IWordLearningAttributes } from './WordLearningAttributes';
-import { WordSessionAttributes } from './WordSessionAttributes';
+import { WordSessionAttributes } from "./WordSessionAttributes";
+/**
+ * @description enum for word type. It can be in 2 states: Introduction and Repeating.
+ */
+export enum WordType {
+    Intro = 0,
+    Repeating = 1
+}
 
+/**
+ * @description The class of representation of a word.
+ */
 export class Word {
-    public generalAttributes: WordGeneralAttributes = new WordGeneralAttributes();
-    public learningAttributes!: IWordLearningAttributes;
-    public sessionAttributes: WordSessionAttributes = new WordSessionAttributes();
-    checkWordAnswer(answer: (Word | string)): boolean {
+    // General attributes of the word.
+    public id!: string;
+    public text!: string;
+    public translation!: string;
+    public description!: string;
+    public transcription!: string;
+    public examples!: string[];
+
+    // Its type.
+    public type: WordType = WordType.Intro;
+    
+    // Atrributes for learning.
+    public introductionCorrectAnswers: number = 0;
+    public repeatingExpirationDate!: Date | null;
+    
+    // Session temporary attributes for a word. It will be initialized in a session.
+    public sessionAttributes: WordSessionAttributes | undefined;
+
+    /**
+     * Method for checking of correctness of the current word.
+     * @param {Word|string} answer 
+     */
+    checkAnswer(answer: (Word | string)): boolean {
         if (typeof answer === 'string') {
-            return this.generalAttributes.translation === answer;
+            return this.text === answer;
+        } else {
+            return this.id === answer.id;
         }
-        return this.generalAttributes.translation === (answer as Word).generalAttributes.translation;
+    }
+
+    /**
+     * @description Self increment of correctness attributes for current word.
+     */
+    incrementCorrectCounter(): void {
+        if (this.type == WordType.Intro) {
+            this.introductionCorrectAnswers ++;
+            //todo: change type
+        } else {
+            let currentDate = new Date(Date.now());
+            currentDate.setDate(currentDate.getDate() + 5);
+            this.repeatingExpirationDate = currentDate;
+        }
     }
 }
