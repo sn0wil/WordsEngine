c68a247af19200645627ad3a63b5875c1707e728
Alexander Denisevich <denisevich.aa@gmail.com>
2020-01-29 13:37:49 +0300

    feat(excercise): class describes excercise for session's client

---
+++ src/Session/Excercise.ts
@@ -0,0 +1,27 @@
+import { Word } from '../Word/Word';
+/**
+ * The type of excercise.
+ */
+export enum ExcerciseType {
+    Radio = 0,
+    Text = 1,
+    OrderCards = 2,
+    Sound = 3
+}
+
+/**
+ * @description Describes a client excercise.
+ */
+export class Excercise {
+    constructor(
+        // The word that should be asked.
+        public question: Word,
+        // POssible answers for question.
+        public answers: Word[] | string[] = [],
+        // Should it be translated into English or not.
+        public toEng: boolean = true,
+        // The type of excercise.
+        public type: ExcerciseType = ExcerciseType.Radio
+    ) {
+    }
+}
