29739627f933508689279be15f208afc316fbd29
Alexander Denisevich <denisevich.aa@gmail.com>
2020-01-30 11:58:23 +0300

    refact(tests): describe blocks added, + 1 more queue push method's test

--- src/Session/Queue.test.ts
+++ src/Session/Queue.test.ts
@@ -4,37 +4,52 @@ const getAnArray = () => {
     return ['one', 'two', 'three'];
 }
 
-test('pop returns first element and length decreases', () => {
-    const array = getAnArray();
-    const queue = new Queue(array);
-    const firstElement = queue.pop();
-    expect(firstElement).toEqual(array[0]);
+describe('pop', () => {
+    it('returns first element and length decreases', () => {
+        const array = getAnArray();
+        const queue = new Queue(array);
+        const firstElement = queue.pop();
+        expect(firstElement).toEqual(array[0]);
+    });
+    
+    it('decreases length', () => {
+        const array = getAnArray();
+        const queue = new Queue(array);
+        queue.pop();
+        expect(queue.length).toEqual(array.length - 1);
+    });
 });
 
-test('pop decreases length', () => {
-    const array = getAnArray();
-    const queue = new Queue(array);
-    queue.pop();
-    expect(queue.length).toEqual(array.length - 1);
+describe('getItem', () => {
+    it('returns element by index', () => {
+        const array = getAnArray();
+        const queue = new Queue(array);
+        const index = 1;
+        expect(queue.getItem(index)).toEqual(array[index]);
+    });
+    
+    it('returns undefined for index >= length', () => {
+        const array = getAnArray();
+        const queue = new Queue(array);
+        expect(queue.getItem(array.length)).toEqual(undefined);
+    });
 });
 
-test('getItem returns element by index', () => {
-    const array = getAnArray();
-    const queue = new Queue(array);
-    const index = 1;
-    expect(queue.getItem(index)).toEqual(array[index]);
-});
+describe('push', () => {
+    test('inserts second element as default', () => {
+        const array = getAnArray();
+        const queue = new Queue(array);
+        const pushingEl = 'after one';
+        queue.push(pushingEl);
+        expect(queue.getItem(1)).toEqual(pushingEl);
+    });
 
-test('getItem returns undefined for index >= length', () => {
-    const array = getAnArray();
-    const queue = new Queue(array);
-    expect(queue.getItem(array.length)).toEqual(undefined);
-});
-
-test('pushSecond inserts second element', () => {
-    const array = getAnArray();
-    const queue = new Queue(array);
-    const pushingEl = 'after one';
-    queue.push(pushingEl);
-    expect(queue.getItem(1)).toEqual(pushingEl);
+    const position = 3;
+    test(`inserts ${position} element`, () => {
+        const array = getAnArray();
+        const queue = new Queue(array);
+        const pushingEl = 'some element';
+        queue.push(pushingEl, position);
+        expect(queue.getItem(position)).toEqual(pushingEl);
+    });
 });
--- src/Word/Word.test.ts
+++ src/Word/Word.test.ts
@@ -8,25 +8,29 @@ const getTestWord = () => {
     return testWord;
 };
 
-test('checkAnswer should return TRUE if words are equal', () => {
-    const testWord = getTestWord();
-    expect(testWord.checkAnswer(testWord)).toEqual(true);
+describe('checkAnswer', () => {
+    it('should return TRUE if words are equal', () => {
+        const testWord = getTestWord();
+        expect(testWord.checkAnswer(testWord)).toEqual(true);
+    });
+    
+    it('should return TRUE if answer is correct', () => {
+        const testWord = getTestWord();
+        expect(testWord.checkAnswer(testWord.translation)).toEqual(true);
+    });
 });
 
-test('checkAnswer should return TRUE if answer is correct', () => {
-    const testWord = getTestWord();
-    expect(testWord.checkAnswer(testWord.translation)).toEqual(true);
-});
-
-test('incrementCorrectCounter should increment introductionCorrectAnswers for Intro word', () => {
-    const testWord = getTestWord();
-    testWord.incrementCorrectCounter();
-    expect(testWord.introductionCorrectAnswers).toEqual(1);
-});
-
-test('incrementCorrectCounter should set repeatingExpirationDate for Repeating word', () => {
-    const testWord = getTestWord();
-    testWord.type = WordType.Repeating;
-    testWord.incrementCorrectCounter();
-    expect(testWord.repeatingExpirationDate).not.toEqual(null);
+describe('incrementCorrectCounter', () => {
+    it('should increment introductionCorrectAnswers for Intro word', () => {
+        const testWord = getTestWord();
+        testWord.incrementCorrectCounter();
+        expect(testWord.introductionCorrectAnswers).toEqual(1);
+    });
+    
+    it('should set repeatingExpirationDate for Repeating word', () => {
+        const testWord = getTestWord();
+        testWord.type = WordType.Repeating;
+        testWord.incrementCorrectCounter();
+        expect(testWord.repeatingExpirationDate).not.toEqual(null);
+    });
 });
