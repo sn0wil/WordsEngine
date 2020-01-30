f8e9983c829dd1baf6a24aa643ac52713a0f15fa
Alexander Denisevich <denisevich.aa@gmail.com>
2020-01-30 13:40:37 +0300

    refact(): deleted src_old

--- src_old/Word/WordGeneralAttributes.ts
+++
@@ -1,8 +0,0 @@
-export class WordGeneralAttributes {
-    public guid!: string;
-    public text!: string;
-    public translation!: string;
-    public description!: string;
-    public transcription!: string;
-    public examples!: string[];
-}
