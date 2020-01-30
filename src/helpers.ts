a119dea1396b666b4e1fa75b6a18841b534d6e80
Alexander Denisevich <denisevich.aa@gmail.com>
2020-01-30 13:31:42 +0300

    feat(helpers): added getTestWords func to recieve an array of Words from json

---
+++ src/helpers.ts
@@ -0,0 +1,34 @@
+import { plainToClass } from "class-transformer";
+import wordsJson from './words.json';
+import { Word } from './Word/Word.js';
+
+/**
+ * @description Returns random value between min and max. Both edges are included.
+ * @param {number} min Minimum value for random.
+ * @param {number} max Maximum value for random.
+ * @returns {number} Random value.
+ */
+export const getRandomNumberFromInterval = (min: number, max: number) => {
+    if (min > max) {
+        [min, max] = [max, min];
+    }
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+/**
+ * Returns the progress of elements that left count in comparison with total count. 
+ * @param {number} elementsLeft Number of left elements.
+ * @param {number} totalElements Total number of elements.
+ * @returns {number} The progress in range 1..100.
+ */
+export const getProgress = (elementsLeft: number, totalElements: number) => {
+    if (elementsLeft === 0 || totalElements === 0) {
+        return 100;
+    }
+    return Math.floor(elementsLeft / totalElements * 100);
+}
+
+/**
+ * @description Returns an array of test words from words.json.
+ */
+export const getTestWords = () => plainToClass(Word, wordsJson);
