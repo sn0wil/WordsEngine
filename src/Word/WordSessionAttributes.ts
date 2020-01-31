b80e1d0c482e5bcaa7ce1094902acf982c0e521a
Alexander Denisevich <denisevich.aa@gmail.com>
2020-02-01 01:42:24 +0300

    reafact(wordSessionAttrs): added description and retirn type for method

--- src/Word/WordSessionAttributes.ts
+++ src/Word/WordSessionAttributes.ts
@@ -1,7 +1,10 @@
+/**
+ * Additional class for word temporary attributes in a session.
+ */
 export class WordSessionAttributes {
     public currectAnswers: number = 0;
     public incorrectAnswers: number = 0;
-    clear() {
+    clear(): void {
         this.currectAnswers = this.incorrectAnswers = 0;
     }
 }
